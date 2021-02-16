9e3cd1497a730e85f7c0a6d402eba29a5e3e442c
jaeseok1111 <jaeseok1111@gmail.com>
2021-02-17 01:41:25 +0900

    create api server project

---
+++ NewsCrawler/NewsCrawler/Controllers/CrawlerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NewsCrawler.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CrawlerController : ControllerBase
+    {
+        private readonly ILogger<CrawlerController> _logger;
+
+        public CrawlerController(ILogger<CrawlerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new JsonResult("Welcome to news crawler");
+        }
+    }
+}
